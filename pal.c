dce43fa790e139182b9475344a314378e3e03214
Andrew Williamson <cs47111@x10.indstate.edu>
2018-09-13 11:51:58 -0400

    adding comments to HW2 program

--- pal.c
+++ pal.c
@@ -1,3 +1,12 @@
+/*
+ * Author: Andrew Williamson
+ * Date: 9/13/18
+ *
+ * Description: Develop a program that accepts a string as a command line argument and determines whether the string is a palindrome
+ * 		duplicated. Works as intended within XV6.
+ *
+ * */
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
@@ -8,31 +17,29 @@
 
 int main(int argc, char *argv[])
 {
-  if(argc < 2){
+  if(argc < 2){   //Checking that a word was given as an argument
     printf(2, "usage: pal word\n");
     exit();
   }  
-  int pflag = 0;
-  int dflag = 0;
-  int len = strlen(argv[1]);
-  char *a = malloc(len+1);
-  strcpy(a, argv[1]);
-	
-
-
-  int h = len-1, l = 0, m = (len+1)/2; 
-  while(l < h) {
+  int pflag = 0; //flag to determine if a palindrome
+  int dflag = 0; //flag to determine if duplicated
+  int len = strlen(argv[1]); //saving length of word
+  char *a = malloc(len+1); //allocating space to copy the string
+  strcpy(a, argv[1]); //copying the word to a new location
+  int h = len-1, l = 0, m = (len+1)/2; //setting count variables, last letter, first letter, middle letter
+  while(l < h) {  //while first does equal last
     if(a[l] != a[h]){
-    	pflag = 1;
+    	pflag = 1; //if not true set palindrome flag 
     }
     if(a[l] != a[m]){
-    	dflag = 1;
+    	dflag = 1; //if not true set duplicated flag
     } 
-    l++;
+    l++; //increment locations within string
     h--;
     m++;
   }
-  if(pflag==0) printf(2,"palindrome.\n");
+  //print statements for outcome of the program
+  if(pflag==0) printf(2,"palindrome.\n"); 
   if(dflag==0) printf(2,"repeated.\n");
   if(pflag==1 && dflag==1) printf(2,"false.\n"); 
   exit();
